6ef324044ad565ca16b9f37a3244a197d8c5435c
comp 2101muskan <muskanthakur860@gmail.com>
2021-10-08 09:13:27 -0700

    Lab 2: Hostname Changer Script

--- bash/hostname-changer.sh
+++ bash/hostname-changer.sh
@@ -5,25 +5,46 @@
 # Test your script, run it on the production server, screenshot that
 # Send your script to your github repo, and submit the URL with screenshot on Blackboard
 
-# Get the current hostname using the hostname command and save it in a variable
-hostname=$(hostname)
+echo "---------------------------------"
+echo "Student: Muskan (200454885)"
 
-# Tell the user what the current hostname is in a human friendly way
-echo "Current hostname is $hostname"
+# Get the current hostname using the hostname command and save it in a variable
+hostNameVariable=`hostname` 
+echo "---------------------------------"
+echo 'Current Host: ' $hostNameVariable # Tell the user what the current hostname is in a human friendly way
 
 # Ask for the user's student number using the read command
-echo "Enter your student number: "
-read num
+echo "---------------------------------"
+echo 'Enter student number:'
+read studentNumber 
+echo "---------------------------------"
+echo 'Your Student Number:'
+echo $studentNumber
 
-# Use that to save the desired hostname of pcNNNNNNNNNN in a variable, where NNNNNNNNN is the student number entered by the user
-newname=pc$num
+futureHostName='pc'
+futureHostName+=$studentNumber # Use that to save the desired hostname of pcNNNNNNNNNN in a variable, where NNNNNNNNN is the student number entered by the user
+echo "---------------------------------"
+echo 'Which Hostname to set:'
+echo $futureHostName
 
-# If that hostname is not already in the /etc/hosts file, change the old hostname in that file to the new name using sed or something similar and
-#     tell the user you did that
+# If that hostname is not already in the /etc/hosts file,
+# change the old hostname in that file to the new name using sed or something similar
+# and tell the user you did that
 #e.g. sed -i "s/$oldname/$newname/" /etc/hosts
-sudo sed -i "s/$hostname/$newname/" /etc/hosts  && echo "Changing your hostname from $hostname to $newname"
+echo "---------------------------------"
+sed -i "s/$hostNameVariable/$futureHostName/" /etc/hosts && echo 'Changed HOSTNAME using sed command'
 
 # If that hostname is not the current hostname, change it using the hostnamectl command and
 #     tell the user you changed the current hostname and they should reboot to make sure the new name takes full effect
 #e.g. hostnamectl set-hostname $newname
-hostnamectl set-hostname $newname && echo "You added the $newname hostname and should reboot your system to apply changes"
+echo "---------------------------------"
+echo 'Checking if the hostname is changed'
+if [[ $hostname != $futureHostName ]]; then
+  hostnamectl set-hostname $futureHostName && echo 'ELSE changing it using HOSTNAMECTL command'
+fi
+
+# Printing Hostname for Verification
+echo "---------------------------------"
+echo 'Now Hostname: ' `hostname` 
+echo "---------------------------------"
+
